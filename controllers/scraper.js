48c262b2c856ad94cefcd476dd745b53f513a7e1
zxcv1337x <michaelhaggard94@gmail.com>
2019-01-02 20:57:23 -0600

    .

--- controllers/scraper.js
+++ controllers/scraper.js
@@ -12,13 +12,14 @@ const Articles = require('../models/articles.js');
 
 // Index
 router.get('/', (req, res) => {
-   Articles.find({}, null, {sort: {_id: -1}}, (err, data) => {
-       if(data.length === 0) {
-           res.render("empty", {msg: "No articles have been scraped. Click Scrape to get the latest News."})
-       } else {
-           res.redirect('index', {articles: data});
-       }
-   })
+    res.redirect('index');
+//    Articles.find({}, null, {sort: {_id: -1}}, (err, data) => {
+//        if(data.length === 0) {
+//            res.render("empty", {msg: "No articles have been scraped. Click Scrape to get the latest News."})
+//        } else {
+//            res.redirect('index', {articles: data});
+//        }
+//    })
 });
 
 // Scrape
